95e991392e775f9d64ece95396d5d4b37551fe71
Nirmal <technktailor@gmail.com>
2024-02-19 22:44:51 +0530

    Added Todos

--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 
     - React, Redux
     - Tailwind CSS
+
+# Table / Data Grid
+
+    - Pagination ( Page Numbers / Arrows )
+    - Sort ( Ascending / Decending )
+    - Search ( Debounce )
+
+# Image Gallery
+
+    - Lazy Loading
+    - Hover Effects
+
+# Lists
+
+    - Show More / Show Less
+    - Infinate Smooth Scroll
--- src/components/plugnplaycomponents/Gallery.js
+++ src/components/plugnplaycomponents/Gallery.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function Gallery() {
   const URL = "https://api.unsplash.com/search/photos";
@@ -10,7 +11,7 @@ function Gallery() {
     const fetchUserList = async () => {
       try {
         setLoading(true);
-        const res = await fetch(`${URL}?query=london?page=1`, {
+        const res = await fetch(`${URL}?query=young?page=1`, {
           method: "GET",
           mode: "cors",
           cache: "no-cache",
@@ -35,15 +36,21 @@ function Gallery() {
 
   const renderImages = photos.map((picture) => {
     return (
-      <div>
-        <img alt={picture.alt_description} src={picture.urls.full} />
+      <div className="w-60 h-60 rounded-md">
+        <img alt={picture.alt_description} src={picture.urls.regular} />
       </div>
     );
   });
 
-  return (
-    <div className="flex flex-row gap-2 pl-4 pr-4 mt-48">{renderImages}</div>
-  );
+  if (loading) {
+    return (
+      <div>
+        <AiOutlineLoading3Quarters />
+      </div>
+    );
+  }
+
+  return <div className="row">{renderImages}</div>;
 }
 
 export default Gallery;
